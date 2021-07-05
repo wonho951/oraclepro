93596c9841b73feeeac26e44ba900326808cce8c
wonho <wonho951@gmail.com>
2021-07-05 13:37:26 +0900

    2021.07.05 (oraclepro)

---
+++ src/com/javaex/phone/PhoneApp.java
@@ -0,0 +1,181 @@
+package com.javaex.phone;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class PhoneApp {
+
+	public static void main(String[] args) {
+
+
+		//스캐너값 받아옴.
+		Scanner sc = new Scanner(System.in);
+				
+		PhoneDao phoneDao = new PhoneDao();
+		List<PersonVo> personList;
+		
+		System.out.println("*************************************************");
+		System.out.println("*           전화번호 관리 프로그램              *");
+		System.out.println("*************************************************");
+		
+		
+		
+		//읽는거 반복 돌리고
+		while (true) {
+
+			PhoneDao.Menu();		
+
+			int num = sc.nextInt();
+			
+			if (num == 6) {
+				System.out.println("*********************************");
+				System.out.println("*	ㄳㄳㄳㄳㄳㄳㄳㄳ		*");
+				System.out.println("*********************************");
+				break;
+			}
+			
+			String name = "";
+			String hp = "";
+			String company = "";
+			int select = 0;
+			int personId = 0;
+			
+			
+			switch (num) {
+			
+			case 1:
+				System.out.println("<1.리스트>");
+				personList = phoneDao.getPersonList();
+				pritnList(personList);
+				break;
+	
+			case 2:
+				System.out.println("<2.등록>");
+				System.out.print(">이름: ");
+				name = sc.nextLine();
+				System.out.print(">휴대전화: ");
+				hp = sc.nextLine();
+				System.out.print(">회사전화: ");
+				company = sc.nextLine();
+				System.out.println("[등록되었습니다.]");
+
+				PersonVo p = new PersonVo(name, hp, company);
+				phoneDao.personInsert(p);
+				break;	
+				
+			case 3:	
+				System.out.println("<3.수정>");
+				System.out.print("번호 > ");
+				personId = sc.nextInt();
+				System.out.print("이름 > ");
+				name = sc.next();
+				System.out.print("휴대전화> ");
+				hp = sc.next();
+				System.out.print("회사번호> ");
+				company = sc.next();
+				System.out.println("[등록되었습니다.]");
+								
+				
+				PersonVo pUpdate = new PersonVo(personId, name, hp, company);
+				phoneDao.personUpdate(pUpdate);
+				break;	
+				
+				
+			case 4:
+				System.out.println("<4.삭제>");	
+				System.out.print("번호 > ");
+				personId = sc.nextInt();
+				
+				PersonVo pDelete = new PersonVo(personId);
+				phoneDao.personDelete(personId);
+				break;
+				
+				
+			case 5:
+				System.out.print("검색어>   ");
+				String srch =sc.next();
+			default:
+				System.out.println("[다시 입력해 주세요]");		
+			}
+			
+		}
+		
+		/*
+		//프로그램 시작 문구 출력
+		Person.mainView();
+
+		
+		while (true) {
+			}
+
+			switch (select) {
+			case 1:
+				System.out.println("<1.리스트>");
+				for (int i = 0; i < pList.size(); i++) {
+					System.out.println(i + 1 + ".	" + pList.get(i).getName() + "	" + pList.get(i).getHp() + "	"
+							+ pList.get(i).getCompany());
+				}
+				break;
+				
+			//등록해야되니까 새로 메모리에 올려주고 추가해.
+			case 2:
+				System.out.println("<2.등록>");
+				System.out.print(">이름: ");
+				String name = sc.next();
+				System.out.print(">휴대전화: ");
+				String hp = sc.next();
+				System.out.print(">회사전화: ");
+				String company = sc.next();
+				System.out.println("[등록되었습니다.]");
+
+				Person p = new Person(name, hp, company);
+				pList.add(p);
+				break;
+				
+			//지우는데 그냥 숫자쓰면 0부터 시작이니까 1지울거면 -1붙여라. 그냥 하면 0번방은 남는다.	
+			case 3:
+				System.out.println("<3.삭제>");
+				System.out.print(">번호: ");
+				int re = sc.nextInt();
+				System.out.println("[삭제되었습니다.]");
+				// -1해야함. 숫자1 썻을때 그냥 1번방 지우는게 되므로 -1해야 0번부터 지울 수 있음.ㅇㅋ?
+				pList.remove(re - 1);
+
+				break;
+				
+			//contains는 문자열 안에 조건에 맞는거 찾아준다.
+			//검색한 후에 검색한거 출력해야 하니까 어떤걸 검색할건지 고민하고 여기선 이름 검색이니까 name해줘라.
+			case 4:
+				System.out.println("<4.검색>");
+				System.out.print(">이름: ");
+				String del = sc.next();
+
+				for (int i = 0; i < pList.size(); i++) {
+
+					if (pList.get(i).getName().contains(del)) {
+						System.out.println(i + 1 + ".	" + pList.get(i).getName() + "	" + pList.get(i).getHp()
+								+ "	" + pList.get(i).getCompany());
+					}
+				}
+
+				break;
+				
+
+		
+*/
+	
+	}
+	
+	//리스트 출력
+	public static void pritnList(List<PersonVo> PersonList) {
+		for (int i = 0; i < PersonList.size(); i++) {
+			PersonVo personVo = PersonList.get(i);
+			System.out.println(personVo.getPersonId() + "\t" + personVo.getName() + "\t" + personVo.getHp() + "\t"
+					+ personVo.getCompany());
+		}
+
+	}
+	
+
+
+}
---
+++ src/com/javaex/phone/PhoneDao.java
@@ -0,0 +1,258 @@
+package com.javaex.phone;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneDao {
+
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String id = "phonedb";
+	private String pw = "phonedb";
+
+	// 생성자
+
+	// 메소드g/s
+
+	// 메소드 - 일반
+
+	// DB연결
+	private void getConnection() {
+
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName(driver);
+
+			// 2. Connection 얻어오기
+			conn = DriverManager.getConnection(url, id, pw);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+	}
+
+	// 자원 정리
+	private void close() {
+
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+	}
+
+	
+	// 전화번호 리스트 가져오기
+	public List<PersonVo> getPersonList() {
+		// 리스트 생성
+		List<PersonVo> personList = new ArrayList<PersonVo>();
+		
+		this.getConnection();
+		
+		
+		try {
+
+		    // 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " select  person_id, ";
+			query += "		   name, ";
+			query += "		   hp,";
+			query += "		   company";
+			query += "		   from person";
+			
+			pstmt = conn.prepareStatement(query);
+			rs = pstmt.executeQuery();
+		    // 4.결과처리
+			
+			while (rs.next()) {
+				int personId = rs.getInt("person_id");
+				String name = rs.getString("name");
+				String hp = rs.getString("hp");
+				String company = rs.getString("company");
+				
+				PersonVo personVo = new PersonVo(personId, name, hp, company);
+				
+				personList.add(personVo);
+			}
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		}
+
+		this.close();
+		
+		return personList;
+	}
+	
+	
+	//사람 등록하기
+	public int personInsert(PersonVo personVo) {
+
+		int count = -1;
+
+		this.getConnection();
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " insert into person ";
+			query += " values(seq_person_id.nextval, ?, ?, ?) ";
+
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, personVo.getName());
+			pstmt.setString(2, personVo.getHp());
+			pstmt.setString(3, personVo.getCompany());
+			
+			count = pstmt.executeUpdate();
+			// 4.결과처리
+			System.out.println(count + "건이 등록되었습니다."); // 등록할땐 개발자가 보는 용도로만.
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+		// 5. 자원정리
+		this.close();
+
+		return count; // 성공갯수 리턴
+	}
+	
+
+	//사람 수정하기
+	public int personUpdate(PersonVo personVo) {
+
+		int count = -1;
+
+		this.getConnection();
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " update person ";
+			query += " set    name = ?, ";
+			query += " 	      hp = ?, ";
+			query += "        company = ? ";
+			query += " where person_id = ? ";
+
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, personVo.getName());
+			pstmt.setString(2, personVo.getHp());
+			pstmt.setString(3, personVo.getCompany());
+			pstmt.setInt(4, personVo.getPersonId());
+
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+			System.out.println(count + "건이 수정되었습니다.");
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+		// 5. 자원정리
+		this.close();
+
+		return count;
+	}
+	
+	
+
+	
+	//사람 삭제하기
+	public int personDelete(int personId) {
+		int count = -1;
+
+		this.getConnection();
+
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " delete from person ";
+			query += " where person_id = ? ";
+
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, personId);
+
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+			System.out.println(count + "건이 삭제되었습니다.");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// 5. 자원정리
+		this.close();
+
+		return count;
+	}
+	
+	
+	
+	//사람 검색하기
+	public int personSearch(String search) {
+		int count = -1;
+
+		this.getConnection();
+
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " select  person_id, ";
+			query += " 		   name, ";
+			query += "  	   hp, ";
+			query += " 		   company ";
+			query += " from person ";
+			query += " where (name || hp || company) like '%" + search + "%' ";
+			query += "   ";
+
+			pstmt = conn.prepareStatement(query);
+			rs = pstmt.executeQuery();
+			
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// 5. 자원정리
+		this.close();
+
+		return count;
+	}
+	
+	
+	
+	//메뉴출력
+	public static void Menu() {
+		System.out.println("");
+		System.out.println("1.리스트" + "	" + "2.등록" + "	" + "3.수정" + "	" + "4.삭제" + "	" + "5.검색" + "	" + "6.종료");
+		System.out.println("-----------------------------");
+		System.out.print(">메뉴선택: ");
+	}
+}
